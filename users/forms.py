0b01c96ae3ff3516ea32fc4fdf6bb0d8bb63a55d
Andrew <shakeyshakespeare0@gmail.com>
2024-03-02 10:15:15 +0000

    Log in done

--- users/forms.py
+++ users/forms.py
@@ -18,3 +18,13 @@ class RegisterForm(UserCreationForm):
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter username'}),
         }
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'}))
+    remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input bigger-checkbox'}))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 
 def register(request):
@@ -11,8 +12,24 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}. Welcome!')
             return redirect('food:index')
-        else:
-            print(form.errors)
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('food:index')
+            else:
+                messages.error(request, 'Invalid username or password')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
