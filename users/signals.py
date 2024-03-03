d64fcdee112db6719673a3c15260e5a1cf3fa7c6
Andrew <shakeyshakespeare0@gmail.com>
2024-03-03 13:11:53 +0000

    Signal for update

--- users/signals.py
+++ users/signals.py
@@ -8,8 +8,11 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        print('Profile created!')
         
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+def save_profile(sender, instance, created, **kwargs):
+    if created == False:
+        instance.profile.save()
+        print('Profile updated!')
